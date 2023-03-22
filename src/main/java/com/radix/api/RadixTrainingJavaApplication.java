9eddc72a4a305dbebb0250902420bc5c7feef121
doularsilva <douglas.silva@radixeng.com.br>
2023-03-21 22:11:36 -0300

    create training project

---
+++ src/main/java/com/radix/api/RadixTrainingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.radix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixTrainingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadixTrainingJavaApplication.class, args);
+	}
+
+}
